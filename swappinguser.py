5aee9a1413459541bcde058042a29beb1b5c86b0
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2019-01-05 16:03:36 +0530

    Add files via upload

---
+++ swappinguser.py
@@ -0,0 +1,12 @@
+# This is a python program for swapping two numbers given by the user.
+# Created By: Mani Agarwal
+# Email : agarwalmani22@gmail.com
+# Date  : 05-01-2019
+
+a=int(input("Enter the 1st number"))
+b=int(input("Enter the 2nd number"))
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)

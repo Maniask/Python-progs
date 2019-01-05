0f4f459435cee503d5b205ae1f37ad1f8281176c
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2019-01-05 16:48:33 +0530

    Add files via upload

---
+++ add2nosuser.py
@@ -0,0 +1,9 @@
+# This is a python program for adding two numbers given by the user.
+# Created By: Mani Agarwal
+# Email : agarwalmani22@gmail.com
+# Date  : 05-01-2019
+
+x=int(input("Enter the 1st number"))
+y=int(input("Enter the 2nd number"))
+z=x+y
+print(z)

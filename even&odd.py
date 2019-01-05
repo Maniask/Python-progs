bb4b72e80d2a9a0c8ec5c9ccccc55e960e2a50b2
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2019-01-05 18:01:32 +0530

    Add files via upload

---
+++ even&odd.py
@@ -0,0 +1,12 @@
+# This is the program for finding if the given number is even or odd.
+# Created By: Mani Agarwal
+# Email : agarwalmani22@gmail.com
+# Date  : 05-01-2019
+x=int(input("Enter the number"))
+result=x%2
+
+if (result==0):
+    print("Number entered by you is even")
+
+if (result==1):
+    print("Number entered by you is odd")

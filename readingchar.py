7406264eb1633fcb902e6065c4311b0374306344
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2019-01-05 17:07:20 +0530

    Add files via upload

---
+++ readingchar.py
@@ -0,0 +1,7 @@
+# This is a python program for getting the first character from the string given by the user as we don't have char in python(insted of char, only string is there).
+# Created By: Mani Agarwal
+# Email : agarwalmani22@gmail.com
+# Date  : 05-01-2019
+
+ch=input("Enter the string")[0]
+print(ch)

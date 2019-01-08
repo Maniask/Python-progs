0de993d4e2b7b22ba26b4864e47e64b92f4fc7ba
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2019-01-08 11:34:49 +0530

    Update Notdivisibleby5.py

--- Notdivisibleby5.py
+++ Notdivisibleby5.py
@@ -1,3 +1,7 @@
+# This is a python program for finding the numbers that are not divisible by 5.
+# Created By: Mani Agarwal
+# Email : agarwalmani22@gmail.com
+# Date  : 08-01-2019
 for i in range(1,21):
     if(i%5!=0):
         print(i)
